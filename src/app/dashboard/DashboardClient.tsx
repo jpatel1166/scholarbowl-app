868a2a217adae9bee7ec4ce7ce8ce3e5d128ea31
Jason Patel <jasonpatel@Mac.lan>
2026-03-01 20:15:59 -0600

    change Practice to Go

--- src/app/dashboard/DashboardClient.tsx
+++ src/app/dashboard/DashboardClient.tsx
@@ -314,7 +314,7 @@ export default function DashboardClient() {
                           background: "white",
                         }}
                       >
-                        Practice
+                        Go
                       </Link>
                     </div>
                   ))}
